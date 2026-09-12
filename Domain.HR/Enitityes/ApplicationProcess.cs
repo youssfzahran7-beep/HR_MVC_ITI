f7cb0141caaa39167bca4f5e16ffa7caa8893dba
X-Link <X-Link@LENA>
2026-09-12 19:13:27 +0300

    Add ApplicationProcess and Candidates entities

---
+++ Domain.HR/Enitityes/ApplicationProcess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.HR.Enitityes
+{
+    public enum ApplicationStage
+    {
+        Applied = 0,
+        Interviewing = 1,
+        Offered = 2,
+        Hired = 3,
+        Rejected = 4
+    }
+    public class ApplicationProcess
+    {
+        public int Id { get; set; }
+
+        public int CandidateId { get; set; }
+
+        public int RecruitmentId { get; set; }
+
+        [Required]
+        public ApplicationStage CurrentStage { get; set; }
+
+        [Required]
+        public DateTime AppliedDate { get; set; }
+    }
+}
